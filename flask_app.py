73c2beb3c2618509623812d153ef6dd579e5314b
lulala123 <linjerry890815@gmail.com>
2020-12-11 02:29:01 +0800

    123

--- flask_app.py
+++ flask_app.py
@@ -12,8 +12,7 @@ connection = pymysql.connect(host=os.environ.get('CLEARDB_DATABASE_HOST'),
                              user=os.environ.get('CLEARDB_DATABASE_USER'),
                              password=os.environ.get('CLEARDB_DATABASE_PASSWORD'),
                              db=os.environ.get('CLEARDB_DATABASE_DB'),
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+                             charset='utf8mb4')
 
 with connection.cursor() as cursor:
     # Create a new record
