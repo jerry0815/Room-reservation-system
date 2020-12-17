0e78c505e40e799139a54fa0b58873c9b04b990d
lulala123 <linjerry890815@gmail.com>
2020-12-17 18:28:28 +0800

    123

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request , render_template
+from flask import Flask, request , render_template , redirect , url_for
 import os
 
 import pymysql
@@ -15,6 +15,12 @@ connection = pymysql.connect(host=os.environ.get('CLEARDB_DATABASE_HOST'),
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+while True:
+    try:
+        connection.ping()
+        break
+    except:
+        connection.ping(True)
 
 with connection.cursor() as cursor:
     # Create a new record
@@ -35,8 +41,20 @@ def hello():
                 result = cursor.fetchone()
                 print(result)
                 return render_template('index.html',name=result["name"] , id = result["id"])
+        elif request.values['send']=='Register':
+            redirect(url_for('register'))
     return render_template("index.html",name="")
 
+@app.route('/register',methods=['POST','GET'])
+def register():
+    if request.method =='POST':
+        if request.values['send']=='Submit':
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO `Account` (`email`, `password` , `name`) VALUES (%s, %s , %s)"
+                cursor.execute(sql, (request.values['email'], request.values['password'] , request.values['name']))
+                result = cursor.fetchone()
+            return render_template('index.html',name=result["name"] , id = result["id"])
+    return render_template("register.html")
 if __name__ == '__main__':
     app.debug = True
     app.run() #啟動伺服器
