f4e521b3c84c4819cb73ac1e26f36cd4b52a595d
jerry0815 <linjerry890815@gmail.com>
2020-12-28 22:13:33 +0800

    DB test

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,6 @@ import os
 import pymysql
 import pymysql.cursors
 
-
 app = Flask(__name__)
 
 # Connect to the database
@@ -15,6 +14,43 @@ connection = pymysql.connect(host=os.environ.get('CLEARDB_DATABASE_HOST'),
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+def insertUser():
+        sql = "INSERT INTO `users` (`userName`, `nickName`,`password` , `email` , `identity` , `banned`) VALUES (%s,%s,%s,%s,%s,%s)"
+        connection.ping(reconnect = True)
+        with connection.cursor() as cursor:
+            cursor.execute(sql,("jerry","lulala","123456798","jerry@gmail.com",'0','0'))
+            connection.commit()
+def insertClassroom():
+    sql = "INSERT INTO `classroom` (`building`, `roomname` , `capacity`) VALUES (%s, %s , %s)"
+    connection.ping(reconnect = True)
+    with connection.cursor() as cursor:
+        cursor.execute(sql,("TR","TR-309","50"))
+        connection.commit()
+
+def showClassroom():
+    sql = "SELECT * FROM `Classroom`"
+    connection.ping(reconnect = True)
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    print(result)
+    return result
+
+def getUser(userName):
+    sql = "SELECT `id`, `email`,`name`FROM `Account` WHERE `userName`=%s"
+    connection.ping(reconnect = True)
+    with connection.cursor() as cursor:
+        cursor.execute(sql,userName)
+        result = cursor.fetchone()
+    print(result)
+    return result
+
+def show():
+    connection.ping(reconnect = True)
+    with connection.cursor() as cursor:
+        cursor.execute("SHOW TABLES")
+        for x in cursor:
+            print(x)
 """
 with connection.cursor() as cursor:
     # Create a new record
@@ -52,6 +88,13 @@ def register():
             connection.commit()
             return redirect(url_for('hello'))
     return render_template("register.html")
+
+@app.route('/testDB',methods=['POST','GET'])
+def testDB():
+    return render_template("testDB.html")
+
 if __name__ == '__main__':
+    insertClassroom()
+    showClassroom()
     app.debug = True
     app.run() #啟動伺服器
