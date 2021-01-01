ff20d3f961a8ec66bece6fad197dd7d673d70e5c
jerry0815 <linjerry890815@gmail.com>
2021-01-01 22:55:24 +0800

    test calendar

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request , render_template , redirect , url_for
 import os
-
 import pymysql
 import pymysql.cursors
+
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 app = Flask(__name__)
 
@@ -364,6 +371,67 @@ def testDB_record():
 def google390a5a29c97e707c():
     return "google-site-verification: google390a5a29c97e707c.html"
 
+
+def get_calendar_service():
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    """
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    """
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_console()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('calendar', 'v3', credentials=creds)
+    return service
+
+@app.route('/calendar')
+def calendar():
+    service = get_calendar_service()
+    # Call the Calendar API
+    print('Getting list of calendars')
+    calendars_result = service.calendarList().list().execute()
+
+    calendars = calendars_result.get('items', [])
+
+    if not calendars:
+        print('No calendars found.')
+    for calendar in calendars:
+        summary = calendar['summary']
+        id = calendar['id']
+        primary = "Primary" if calendar.get('primary') else ""
+        print("%s\t%s\t%s" % (summary, id, primary))
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    
+    events_result = service.events().list(calendarId="primary", timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    tmp = []
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        tmp.append([start + " " + event['summary']])
+        print(start, event['summary'])
+    return tmp
 if __name__ == '__main__':
     app.debug = True
     app.run() #啟動伺服器
--- test_calendar.py
+++ test_calendar.py
@@ -6,11 +6,11 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
-def main():
+def get_calendar_service():
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -27,18 +27,74 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+                'credentials_local.json', SCOPES)
+            creds = flow.run_console()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
-
     service = build('calendar', 'v3', credentials=creds)
+    return service
+def main():
+    service = get_calendar_service()
+    # Call the Calendar API
+    print('Getting list of calendars')
+    calendars_result = service.calendarList().list().execute()
 
+    calendars = calendars_result.get('items', [])
+
+    if not calendars:
+        print('No calendars found.')
+    for calendar in calendars:
+        summary = calendar['summary']
+        id = calendar['id']
+        primary = "Primary" if calendar.get('primary') else ""
+        print("%s\t%s\t%s" % (summary, id, primary))
+
+    """
+    event = {
+    'summary': 'Google calendar my test',
+    'location': '800 Howard St., San Francisco, CA 94103',
+    'description': 'A chance to hear more about Google\'s developer products.',
+    'start': {
+        'dateTime': '2021-01-10T09:00:00-07:00',
+        'timeZone': 'Asia/Taipei',
+    },
+    'end': {
+        'dateTime': '2021-01-10T09:00:00-07:00',
+        'timeZone': 'Asia/Taipei',
+    },
+    'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+    ],
+    'attendees': [
+        {'email': 'jerrylulala@gmail.com'}
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+    ],
+    'reminders': {
+        'useDefault': False,
+        'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10},
+        ],
+    },
+    }
+    
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
+    
+
+
+    calendar_list = service.calendarList().list().execute()
+    print(len(calendar_list))
+    for i in calendar_list["items"]:
+        print(i["id"])
+    #print(calendar_list["items"][0]['id'])
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
+    
+    events_result = service.events().list(calendarId="primary", timeMin=now,
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
@@ -48,7 +104,7 @@ def main():
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
-
+    """
 
 if __name__ == '__main__':
     main()
