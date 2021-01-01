adb92834890fa8417346de14b5596d0123db5df9
jerry0815 <linjerry890815@gmail.com>
2021-01-02 01:16:57 +0800

    test insert calendar

--- flask_app.py
+++ flask_app.py
@@ -435,7 +435,7 @@ def test_api_request():
     service = googleapiclient.discovery.build(
         API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
-    result = insertEvent(service=service , title = "test insert calendar" , startDate = "2021-01-10" , startSection=5 , endDate = "2021-01-10" , endSection=8,participants=["linjerry890815@gmail.com"])
+    result = insertEvent(service=service , title = "test insert calendar" , roomname = "TR-313",startDate = "2021-01-10" , startSection=5 , endDate = "2021-01-10" , endSection=8,participants=["linjerry890815@gmail.com"])
     print("insert result eventID: " + result)
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
