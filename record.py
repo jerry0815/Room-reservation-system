70c8d0f66901b33a8be372b6a67b1ab944beb2e9
jerry0815 <linjerry890815@gmail.com>
2021-01-07 00:12:47 +0800

    check if record conflict

--- record.py
+++ record.py
@@ -8,6 +8,22 @@ import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import flask
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = "credentials.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/calendar' , 'https://www.googleapis.com/auth/calendar.events']
+API_SERVICE_NAME = 'calendar'
+API_VERSION = 'v3'
+# Note: A secret key is included in the sample so that it works.
+# If you use this code in your application, replace this with a truly secret
+# key. See https://flask.palletsprojects.com/quickstart/#sessions.
+app.secret_key = "My key"
+
+
 def calendar_process(data , calendar_type):
     """
     if 'credentials' not in flask.session:
