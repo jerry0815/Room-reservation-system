4d1bc7b3cc9e742b3b19cf4ab536e54a31b6be72
jerry0815 <linjerry890815@gmail.com>
2021-01-07 00:12:37 +0800

    check if record conflict

--- flask_app.py
+++ flask_app.py
@@ -145,19 +145,6 @@ def deleteEvent(service , startDate , startSection):
     print(event)
     service.events().delete(calendarId = 'primary' , eventId = event['id'] , sendUpdates = "all").execute()
 
-# This variable specifies the name of a file that contains the OAuth 2.0
-# information for this application, including its client_id and client_secret.
-CLIENT_SECRETS_FILE = "credentials.json"
-
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection.
-SCOPES = ['https://www.googleapis.com/auth/calendar' , 'https://www.googleapis.com/auth/calendar.events']
-API_SERVICE_NAME = 'calendar'
-API_VERSION = 'v3'
-# Note: A secret key is included in the sample so that it works.
-# If you use this code in your application, replace this with a truly secret
-# key. See https://flask.palletsprojects.com/quickstart/#sessions.
-app.secret_key = "My key"
 @app.route('/test')
 def test_api_request():
     """
